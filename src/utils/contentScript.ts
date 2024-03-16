92669be3f9ddd8e7ed67d04c9436e9991020ce0d
Robin Simonklein <robin.simonklein@mezcalito.fr>
2024-03-16 21:37:28 +0100

    fix: Prevent devtools crash

--- src/utils/contentScript.ts
+++ src/utils/contentScript.ts
@@ -1,7 +1,11 @@
 export const sendMessage = async (message: unknown) => {
   const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
-  if (!tab.id) throw new Error('Tab id is not defined');
-  await chrome.tabs.sendMessage(tab.id, message);
+  if (!tab?.id) throw new Error('Tab id is not defined');
+  try {
+    await chrome.tabs.sendMessage(tab.id, message);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const executeAction = async (name: string, args?: Record<string, unknown>) => {
--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -1,4 +1,5 @@
 export const getElementSelectorString = (element: Element) => {
+  if (!element) return '';
   let str = element.localName;
   if (element.id) str += '#' + element.id;
   if (element.classList.length) {
