fc639417b5a3b0d954218f6cfd4c6392ffbcbdc7
Robin Simonklein <robin.simonklein@mezcalito.fr>
2024-03-29 17:11:53 +0100

    feat: Better unavailable view style

--- src/components/stimulus/StimulusUnavailable.vue
+++ src/components/stimulus/StimulusUnavailable.vue
@@ -1,22 +1,17 @@
 <template>
-  <div class="absolute inset-0 flex flex-col items-center justify-center px-2">
+  <div class="absolute inset-0 flex flex-col items-center justify-center px-4">
     <div class="inline-flex items-center">
-      <StimulusLogo class="h-5 w-5 sm:h-8 sm:w-8" />
-      <p class="ml-2 text-xl font-bold sm:text-3xl">
-        <span>Stimulus</span>
-        <span class="ml-1 text-stimulus">DevTools</span>
-      </p>
+      <StimulusDevToolsLogo class="w-[320px] max-w-full" />
     </div>
     <div class="mt-14 text-center">
       <p class="text-muted-foreground">Stimulus is not detected on this page.</p>
       <p class="text-muted-foreground">
-        Add your application to <CodeInline code="window.Stimulus" language="javascript" /> to enable devtools.
+        Add your application to <span class="font-mono text-code-purple">window.Stimulus</span> to enable devtools.
       </p>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import StimulusLogo from '@/components/StimulusLogo.vue';
-import CodeInline from '@/components/core/code/CodeInline.vue';
+import StimulusDevToolsLogo from '@/components/StimulusDevToolsLogo.vue';
 </script>
